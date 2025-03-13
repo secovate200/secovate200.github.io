a1aef602466c8525e6b61153c2cca4a65c18706e
secovate200 <secovate200@gmail.com>
2025-03-13 15:35:47 +0900

    2025-03-13 post정리

--- hello.c
+++ hello.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 int main(void){
-    printf("Hello World");
-    return 0;
+  int count =0;
+  char name[20]="Hello World";
+  for (int i=0; i<sizeof(name)/sizeof(char); i++){
+    if(name[i]=='l'){
+      count ++;
+    }
+  }
+  printf("l의 개수는 %d 개 입니다",count);
+  return 0;
 }
