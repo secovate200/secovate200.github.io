cd3f7e7d3bf53afce3577a1a4b65c5ddffc39d24
secovate200 <secovate200@gmail.com>
2025-03-15 14:47:14 +0900

    2025-03-15 다차원배열,포인터배열

--- hello.c
+++ hello.c
@@ -1,10 +1,11 @@
-#include<stdio.h>
-void add(int *a){
- *a =(*a)+10;
+#include <stdio.h>
+
+int main(void) {
+  int a[2][5] = { { 1, 2, 3, 4, 5 },
+                  { 6, 7, 8, 9, 10 } };
+    int (*p)[5]= &a[1];
+    for( int i=0; i<sizeof(*p)/sizeof(int); i++){
+      printf("%d ",p[0][i]);
+    }
+    return 0;
 }
- int main(void) {
-  int a=7;
-  add(&a);
-  printf("%d",a);
-  return 0;
- }
