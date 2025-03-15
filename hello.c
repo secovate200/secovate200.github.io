2a3647dd4b6a0b9d065eda5244c3febb4ac67309
secovate200 <secovate200@gmail.com>
2025-03-15 10:41:50 +0900

    포스트 작성자 출력 오류 해결

--- hello.c
+++ hello.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
 int main(void){
-  int arr[]={1,2,3,4,5,6,7,8,9,10};
-  int *b=arr;
-  printf("%d",b[1]);
+
+  char a[100];
+  gets(a);
+
+
+  printf("%s\n",a);
+
   return 0;
 }
